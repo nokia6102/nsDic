064d569b6b52b35c858f3c6494b7dab2608754df
1 <nokia6102@gmail.com>
2018-02-06 15:35:47 +0800

    Initial Commit

---
+++ nsdic/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  nsdic
+//
+//  Created by Primax on 2018/2/6.
+//  Copyright © 2018年 Primax. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
